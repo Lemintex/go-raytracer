b2569dc983dc8d0e8e464c1301d8a343232b3b42
Lemintex <aaron_nobile@hotmail.co.uk>
2024-07-27 20:37:38 +0100

    Added true lambertian reflection

--- ray.go
+++ ray.go
@@ -17,7 +17,7 @@ func (r Ray) Color(world HittableList, bounce int) Vec3 {
 	}
 	didHit, hit := world.Hit(r, Interval{0.001, math.Inf(1)})
 	if didHit {
-		direction := RandomVec3OnHemisphere(hit.Normal)
+		direction := hit.Normal.Add(RandomUnitVec3())
 		r := Ray{hit.Point, direction}
 		return r.Color(world, bounce-1).MulScalar(0.5)
 	}
