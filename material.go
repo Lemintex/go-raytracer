de83a35f4b2296e77bd76ed063794664383d8187
Lemintex <aaron_nobile@hotmail.co.uk>
2024-07-28 02:48:59 +0100

    Added metal fuzziness

--- material.go
+++ material.go
@@ -19,10 +19,11 @@ func (l Lambertian) Scatter(ray Ray, hit HitInfo) (bool, Ray, Vec3) {
 
 type Metal struct {
 	Albedo Vec3
+	Fuzz   float64
 }
 
 func (m Metal) Scatter(ray Ray, hit HitInfo) (bool, Ray, Vec3) {
-	reflected := ray.Direction.Unit().Reflect(hit.Normal)
+	reflected := ray.Direction.Unit().Reflect(hit.Normal).Add(RandomVec3InUnitSphere().MulScalar(m.Fuzz))
 	scattered := Ray{hit.Point, reflected}
-	return true, scattered, m.Albedo
+	return scattered.Direction.Dot(hit.Normal) > 0, scattered, m.Albedo
 }
