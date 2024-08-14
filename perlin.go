254a57eb4368e9f87149e7e1e48cd8b2cb37ea1c
Lemintex <aaron_nobile@hotmail.co.uk>
2024-08-14 22:11:10 +0100

    Improved noise smoothing by using random vectors on lattice points

--- perlin.go
+++ perlin.go
@@ -3,19 +3,19 @@ package main
 import "math"
 
 type Perlin struct {
-	PointCount   int
-	RandomFloats []float64
-	PermX        []int
-	PermY        []int
-	PermZ        []int
+	PointCount int
+	RandomVecs []Vec3
+	PermX      []int
+	PermY      []int
+	PermZ      []int
 }
 
 func NewPerlin() Perlin {
 	p := Perlin{}
 	p.PointCount = 256
-	p.RandomFloats = make([]float64, p.PointCount)
+	p.RandomVecs = make([]Vec3, p.PointCount)
 	for i := range p.PointCount {
-		p.RandomFloats[i] = RandomFloat()
+		p.RandomVecs[i] = RandomUnitVec3()
 	}
 	p.PermX = PerlinGeneratePerm()
 	p.PermY = PerlinGeneratePerm()
@@ -27,13 +27,13 @@ func (p Perlin) Noise(vec Vec3) float64 {
 	u, v, w := vec.X-math.Floor(vec.X), vec.Y-math.Floor(vec.Y), vec.Z-math.Floor(vec.Z)
 	u, v, w = u*u*(3-2*u), v*v*(3-2*v), w*w*(3-2*w)
 	i, j, k := int(math.Floor(vec.X)), int(math.Floor(vec.Y)), int(math.Floor(vec.Z))
-	c := make([][][]float64, 2)
+	c := make([][][]Vec3, 2)
 	for di := 0; di < 2; di++ {
-		c[di] = make([][]float64, 2)
+		c[di] = make([][]Vec3, 2)
 		for dj := 0; dj < 2; dj++ {
-			c[di][dj] = make([]float64, 2)
+			c[di][dj] = make([]Vec3, 2)
 			for dk := 0; dk < 2; dk++ {
-				c[di][dj][dk] = p.RandomFloats[p.PermX[(i+di)&255]^p.PermY[(j+dj)&255]^p.PermZ[(k+dk)&255]]
+				c[di][dj][dk] = p.RandomVecs[p.PermX[(i+di)&255]^p.PermY[(j+dj)&255]^p.PermZ[(k+dk)&255]]
 			}
 		}
 	}
@@ -69,12 +69,12 @@ func PerlinPermute(p []int, n int) []int {
 	return p
 }
 
-func PerlinInterp(c [][][]float64, u, v, w float64) float64 {
+func PerlinInterp(c [][][]Vec3, u, v, w float64) float64 {
 	accum := 0.0
 	for i := 0; i < 2; i++ {
 		for j := 0; j < 2; j++ {
 			for k := 0; k < 2; k++ {
-				accum += c[i][j][k] * (float64(i)*u + (1-float64(i))*(1-u)) * (float64(j)*v + (1-float64(j))*(1-v)) * (float64(k)*w + (1-float64(k))*(1-w))
+				accum += (float64(i)*u + float64(1-i)*(1-u)) * (float64(j)*v + float64(1-j)*(1-v)) * (float64(k)*w + float64(1-k)*(1-w)) * c[i][j][k].Dot(Vec3{u - float64(i), v - float64(j), w - float64(k)})
 			}
 		}
 	}
